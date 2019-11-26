21defc064d72f15423c25a2ddba868d9fe5dae03
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-11-26 19:53:11 +0100

    Starting of work
    Starting of work

---
+++ SeleniumProjectWF/SeleniumProjectWF/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace SeleniumProjectWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCountry = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textLang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textExpQ = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textRealQ = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textUrl
+            // 
+            this.textUrl.Location = new System.Drawing.Point(208, 52);
+            this.textUrl.Name = "textUrl";
+            this.textUrl.Size = new System.Drawing.Size(100, 20);
+            this.textUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Country";
+            // 
+            // textCountry
+            // 
+            this.textCountry.Location = new System.Drawing.Point(59, 110);
+            this.textCountry.Name = "textCountry";
+            this.textCountry.Size = new System.Drawing.Size(100, 20);
+            this.textCountry.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Language";
+            // 
+            // textLang
+            // 
+            this.textLang.Location = new System.Drawing.Point(59, 175);
+            this.textLang.Name = "textLang";
+            this.textLang.Size = new System.Drawing.Size(100, 20);
+            this.textLang.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Exp Qnt. Vacancies";
+            // 
+            // textExpQ
+            // 
+            this.textExpQ.Location = new System.Drawing.Point(59, 240);
+            this.textExpQ.Name = "textExpQ";
+            this.textExpQ.Size = new System.Drawing.Size(100, 20);
+            this.textExpQ.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(217, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 62);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Real Qnt. Vacancies";
+            // 
+            // textRealQ
+            // 
+            this.textRealQ.Location = new System.Drawing.Point(351, 110);
+            this.textRealQ.Name = "textRealQ";
+            this.textRealQ.Size = new System.Drawing.Size(100, 20);
+            this.textRealQ.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 25);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Result";
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(351, 175);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(100, 20);
+            this.textResult.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 322);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textRealQ);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textExpQ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textLang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCountry);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textUrl);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test veem.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCountry;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textLang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textExpQ;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textRealQ;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textResult;
+    }
+}
+
---
+++ SeleniumProjectWF/SeleniumProjectWF/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+
+namespace SeleniumProjectWF
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //https://careers.veeam.com/
+            if (methodCheckTextBox())
+            {
+                Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+                Browser.Manage().Window.Maximize();
+                try
+                {
+                    Browser.Navigate().GoToUrl(textUrl.Text);
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show("exception : " + ee);
+                }
+                
+            }
+
+        }
+
+        bool methodCheckTextBox()
+        {
+            if (textUrl.TextLength > 10 && textCountry.TextLength > 3 && textLang.TextLength > 3 && textExpQ.TextLength > 0)
+            { return true; }
+            else { return false; }
+        }
+
+
+    }
+}
